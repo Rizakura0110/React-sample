78719a881b2f4b2157780e1cc5da25f0ee9df91d
Rizakura0110 <rasakura0110@gmail.com>
2022-08-24 23:05:33 +0900

    問題あり要修正

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Hello from './components/Hello'
+import Hello from './components/useStateTest'
+import Counter from './components/useStateTest';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* App から　Helloに*/}
-    <Hello />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root') as HTMLElement
 );
